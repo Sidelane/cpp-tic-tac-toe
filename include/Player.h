f76ba8223fdbe848ef11c1f3f6bf04a39c9710e8
Sidelane <36916365+Sidelane@users.noreply.github.com>
2020-02-17 14:33:20 +0100

    Create Player.h

---
+++ include/Player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+
+
+class Player
+{
+    public:
+        Player(std::string player_char);
+
+        bool GethasTurned() { return hasTurned; }
+        void SethasTurned(bool val) { hasTurned = val; }
+        std::string Getplayer() { return player; }
+
+    private:
+        bool hasTurned;
+        std::string player;
+};
+
+#endif // PLAYER_H
