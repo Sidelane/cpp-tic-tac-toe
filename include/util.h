e710a49c4b0034824ef864a0a37df8a7dd006ba5
Sidelane <36916365+Sidelane@users.noreply.github.com>
2020-02-17 15:37:51 +0100

    Add files via upload

---
+++ include/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H_INCLUDED
+#define UTIL_H_INCLUDED
+
+void random_start(Player&, Player&);
+void init_board(Board&);
+void set_answer(Board &, Player &, std::vector<Player> &);
+int check_if_end(Board &, Player &);
+
+#endif // UTIL_H_INCLUDED
